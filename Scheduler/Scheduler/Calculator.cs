fd1b097a13407248c871cfa5eea74196c93761eb
inesd <inesdelfdez@gmail.com>
2021-10-10 19:49:01 +0200

    Eliminar proyecto de Windows

---
+++ Scheduler/Scheduler/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public static class Calculator
+    {
+        public static Schedule CreateSchedule(Configuration configuration)
+        {
+            Validator.ValidateStartEndDates(configuration.StartDate, configuration.EndDate);
+
+            Schedule schedule = new Schedule(configuration.Type, configuration.StartDate, configuration.EndDate);
+
+            DateTime referenceDate = Calculator.GetReferenceDate(configuration.CurrentDate, configuration.Date);
+            
+            DateTime? nextDate = null;
+            if (Calculator.DateInLimits(referenceDate, configuration.StartDate, configuration.EndDate))
+            {
+                nextDate = Calculator.GetNextDate(configuration.Type, referenceDate, configuration.NumberOfDays);
+            }
+            
+            schedule.NextDate = nextDate;
+
+            return schedule;
+        }
+
+        private static DateTime GetReferenceDate(DateTime currentDate, DateTime? date)
+        {
+            if(date.HasValue == false || date.Value < currentDate)
+            {
+                return currentDate;
+            }
+            return date.Value;
+        }
+
+        public static bool DateInLimits(DateTime referenceDate, DateTime? startDate, DateTime? endDate)
+        {
+            bool validDateStart = startDate.HasValue == false || startDate.Value <= referenceDate;
+            bool validDateEnd = endDate.HasValue == false || endDate.Value >= referenceDate;
+            if((validDateEnd && validDateStart) == false)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static DateTime GetNextDate(Type type, DateTime referenceDate, int numberOfDays)
+        {
+            if (type == Type.Once)
+            {
+                return referenceDate;
+            }
+            else
+            {
+                return referenceDate.AddDays(numberOfDays);
+            }
+        }
+    }
+}
---
+++ Scheduler/Scheduler/Schedule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public class Schedule
+    {
+        public Schedule(Type typeOfSchedule, DateTime? startDate, DateTime? endDate)
+        {
+            this.TypeOfSchedule = typeOfSchedule;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? NextDate { get; set; }
+        public Type TypeOfSchedule { get; set; }
+    }
+}
