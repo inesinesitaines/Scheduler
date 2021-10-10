3cc27e07caf0e1ba1a43aa28995bf0012005617a
inesd <inesdelfdez@gmail.com>
2021-10-10 20:21:57 +0200

    Cambio de nombre de proyectos y referencias

---
+++ Scheduler/Scheduler/Validator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public static class Validator
+    {
+        public static void ValidateStartEndDates(DateTime? startDate, DateTime? endDate)
+        {
+            if(startDate.HasValue && endDate.HasValue && startDate > endDate)
+            {
+                throw new Exception("Start date should be smaller than the end date.");
+            }
+        }
+    }
+}
